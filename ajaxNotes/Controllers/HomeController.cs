a33c9c2e5914a2695461e7a43f0d5295125c6f1f
Bald Mike <baldmike312@gmail.com>
2017-09-14 15:55:28 -0500

    ajaxNotes

--- ajaxNotes/Controllers/HomeController.cs
+++ ajaxNotes/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using DbConnection;
 
 namespace ajaxNotes.Controllers
 {
@@ -13,7 +14,38 @@ namespace ajaxNotes.Controllers
         [Route("")]
         public IActionResult Index()
         {
+            string query = $"SELECT * FROM NOTES ORDER BY ID DESC";
+            List<Dictionary<string, object>> allNotes = DbConnector.Query(query);
+            ViewBag.notes = allNotes;
             return View();
+        }
+
+        [HttpPost]
+        [Route("/addNote")]
+        public string AddNote(string title)
+        {
+            string query = $"INSERT INTO NOTES (TITLE) VALUES ('{title}')";
+            var result = DbConnector.Insert(query);
+            return result;
+        }
+
+        [HttpPost]
+        [Route("/updateNote")]
+        public string UpdateNote(string id, string note)
+        {
+            string query = $"UPDATE NOTES SET NOTE = '{note}' WHERE ID = {id}";
+            DbConnector.Execute(query);
+            return "ok";
+        }
+
+        [HttpPost]
+        [Route("/delete")]
+        public string delete(string ID)
+        {
+            string query = $"DELETE FROM NOTES WHERE ID = '{ID}'";
+            DbConnector.Execute(query);
+            //return RedirectToAction("Index");
+            return "ok";
         }
     }
 }
