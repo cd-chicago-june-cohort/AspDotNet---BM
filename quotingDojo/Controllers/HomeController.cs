85dc4e6ace62e77cb1b5a062865e2f1f83f6d0c6
Bald Mike <baldmike312@gmail.com>
2017-09-13 20:30:57 -0500

    quoting dojo - delete route

--- quotingDojo/Controllers/HomeController.cs
+++ quotingDojo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using DbConnection;
 
 namespace quotingDojo.Controllers
 {
@@ -13,7 +14,35 @@ namespace quotingDojo.Controllers
         [Route("")]
         public IActionResult Index()
         {
+            
             return View();
+        }
+
+        [HttpGet]
+        [Route("/quotes")]
+        public IActionResult Quotes()
+        {
+            List<Dictionary<string, object>> AllQuotes = DbConnector.Query("SELECT * FROM quotes");
+            ViewBag.Quotes = AllQuotes;
+            return View("quotes");
+        }
+
+        [HttpPost]
+        [Route("/addQuote")]
+        public IActionResult addQuote(string name, string quote)
+        {
+            DbConnector.Execute($"INSERT INTO quotes (name, quote, created_date) VALUES ('{name}', '{quote}', now())");
+            
+            return RedirectToAction ("quotes");
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public IActionResult delete(int id)
+        {
+            DbConnector.Execute($"DELETE FROM quotes where id = {id}");
+            
+            return RedirectToAction ("quotes");
         }
     }
 }
